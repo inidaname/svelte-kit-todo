5d46aa0e18928f7e37c6f54f102e37a7d1e54031
Hassan Sani <saniyhassan@gmail.com>
2022-07-05 12:35:09 +0100

    finilize on the blog

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,10 +2,16 @@
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-// and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		user?: { username: string; id: string };
+	}
+
 	// interface Platform {}
-	// interface Session {}
+
+	interface Session {
+		user?: { username: string };
+	}
+
 	// interface Stuff {}
 }
